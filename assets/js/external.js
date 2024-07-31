49a1f5e34984495bbefd4dbbe84d53319d25e7b0
sametsahinnet <>
2024-07-31 16:18:19 +0300

    collect-vars demo

---
+++ assets/js/external.js
@@ -0,0 +1,5 @@
+const API_KEY = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6";
+var userPassword = "correct-horse-battery-staple";
+let DATABASE_URL = "mongodb://username:password@host:port/database";
+const STRIPE_SECRET_KEY = "sk_live_1234567890abcdefghijklmnop";
+var applicationName = "MyAwesomeApp";
